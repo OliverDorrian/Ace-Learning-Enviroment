dc60ce335e3021ab7ef590a32659b110a476e7cd
OliverDorrian <dorrianolfc@gmail.com>
2022-03-22 19:27:13 +0000

    Created A downloads section

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-window.addEventListener(
-    "scroll",
-    () => {
-        document.body.style.setProperty(
-            "--scroll",
-            window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-        );
-        document.body.style.setProperty(
-            "--scroll-per",
-            `${(window.pageYOffset / document.body.offsetHeight) * 200}%`
-        );
-    },
-    false
-);
+
 
 document.getElementById("centerDivClick").addEventListener("click", function(){
     document.getElementById('CenteringDivs').scrollIntoView();
